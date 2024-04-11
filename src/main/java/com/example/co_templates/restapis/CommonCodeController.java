a488b69cce0c3610078aa20cb8b87a6d7ea75ac7
Sanghun Oh <otter.oh@gmail.com>
2024-04-11 05:41:18 +0000

    first commit

---
+++ src/main/java/com/example/co_templates/restapis/CommonCodeController.java
@@ -0,0 +1,38 @@
+package com.example.co_templates.restapis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.co_templates.services.CommonCodeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class CommonCodeController {
+
+    @Autowired CommonCodeService commonCodeService;
+    
+    @GetMapping("/r/commonCode/list/{pageNumber}")
+    public ResponseEntity<List<HashMap<String, Object>>> list(@PathVariable("pageNumber") Integer pageNumber) {
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+        // Call Service with Pure Java
+        // CommonCodeService commonCodeService = new CommonCodeService();
+        itemList = commonCodeService.list(pageNumber);
+
+        return ResponseEntity.ok().body(itemList);
+    }
+
+    @GetMapping("/r/commonCode/view/{pk_id}")
+    public ResponseEntity<HashMap<String, Object>> view(@PathVariable("pk_id") Integer pkId) {
+        HashMap<String, Object> itemDetails = new HashMap<>();
+        itemDetails = commonCodeService.view(pkId);
+
+        return ResponseEntity.ok().body(itemDetails);
+    }
+}
---
+++ src/main/java/com/example/co_templates/services/CommonCodeService.java
@@ -0,0 +1,39 @@
+package com.example.co_templates.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonCodeService {
+    public ArrayList<HashMap<String, Object>> list(Integer pageNumber){
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+ 
+        HashMap<String, Object> item = new HashMap<>();
+        item.put("PK_ID", 1);
+        item.put("FK_ID", 10);
+        item.put("NAME", "Excalibur");
+        itemList.add(item);
+    
+        item = new HashMap<>();
+        item.put("PK_ID", 2);
+        item.put("FK_ID", 20);
+        item.put("NAME", "Dragon Sword");
+        itemList.add(item);
+
+        return itemList;
+    }
+
+    public HashMap<String, Object> view(Integer pkId){
+        HashMap<String, Object> itemDetails = new HashMap<>();
+
+        String fkId = "FK_0382";
+        String name = "Commons";
+        itemDetails.put("PK_ID", pkId);
+        itemDetails.put("FK_ID", fkId);
+        itemDetails.put("NAME", name);
+
+        return itemDetails;
+    }
+}
